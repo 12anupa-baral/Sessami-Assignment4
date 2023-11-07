d6cac49e27e7efd1e7785a0b7dc56e1288a46d69
Anupa Baral <baralanupa988@gmail.com>
2023-11-07 18:19:21 +0545

    background added

--- script.js
+++ script.js
@@ -3,31 +3,38 @@ const modal = document.getElementById("modal");
 const downButton = document.getElementById("downButton");
 const cancel = document.getElementById("cancel");
 const icon = document.getElementById("icon");
+const container = document.getElementById("container");
 
 clickHere.addEventListener("click", function () {
   modal.style.display = "block";
+  container.style.background = "rgba(0, 0, 0, 0.5)";
 });
 
 downButton.addEventListener("click", function () {
   modal.style.display = "none";
+  container.style.background = "rgba(0, 0, 0, 0)";
 });
 
 cancel.addEventListener("click", function () {
   modal.style.display = "none";
+  container.style.background = "rgba(0, 0, 0, 0)";
 });
 
 icon.addEventListener("click", function () {
   modal.style.display = "none";
+  container.style.background = "rgba(0, 0, 0, 0)";
 });
 
 window.addEventListener("click", function (event) {
   if (event.target === modal) {
     modal.style.display = "none";
+    container.style.background = "rgba(0, 0, 0, 0)";
   }
 });
 
 clickHere.addEventListener("click", function () {
   if (display === "block") {
     clickHere.style.display = "none";
+    container.style.background = "rgba(0, 0, 0, 0)";
   }
 });
