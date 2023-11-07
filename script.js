4f2473607fd78f91e61cfb460bf0bdf97be4bf6c
Anupa Baral <baralanupa988@gmail.com>
2023-11-07 18:55:26 +0545

    added function

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 const clickHere = document.getElementById("clickHere");
 const modal = document.getElementById("modal");
-const downButton = document.getElementById("downButton");
-const cancel = document.getElementById("cancel");
-const icon = document.getElementById("icon");
 const container = document.getElementById("container");
+
+function closeModal() {
+  modal.style.display = "none";
+  container.style.background = "";
+}
 
 clickHere.addEventListener("click", function () {
   modal.style.display = "block";
   container.style.background = "rgba(0, 0, 0, 0.5)";
 });
 
-downButton.addEventListener("click", function () {
-  modal.style.display = "none";
-});
+const closeElements = [
+  modal,
+  document.getElementById("downButton"),
+  document.getElementById("cancel"),
+  document.getElementById("icon"),
+];
 
-cancel.addEventListener("click", function () {
-  modal.style.display = "none";
-});
-
-icon.addEventListener("click", function () {
-  modal.style.display = "none";
-});
-
-window.addEventListener("click", function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-});
-
-clickHere.addEventListener("click", function () {
-  if (display === "block") {
-    clickHere.style.display = "none";
-  }
+closeElements.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    if (event.target === element || event.target === modal) {
+      closeModal();
+    }
+  });
 });
